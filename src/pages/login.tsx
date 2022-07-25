d28f56d9cd597db9a90df85879a0543b72462dab
Mohammad Beheshti <m.beheshti@pragmateam.dev>
2022-07-25 09:37:29 +0430

    fix: remove unused imports

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,6 @@
 import {useForm} from "react-hook-form";
 import {useAuthControllerLogin} from "../queries/apiComponents";
 import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
 export default function Login() {
   const navigate = useNavigate();
   const {
